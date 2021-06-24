271b6130dd7ba37b70976b4a457f068a11024cf8
Torin Hawkins <torincyie13@gmail.com>
2021-06-23 19:50:32 -0500

    Add project files.

---
+++ searchingBooksDatabase/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace searchingBooksDatabase
+{
+    partial class frmBooks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.grdBooks = new System.Windows.Forms.DataGridView();
+            this.sQLBooksDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sQLBooksDBDataSet = new searchingBooksDatabase.SQLBooksDBDataSet();
+            this.sQLBooksDBDataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSetBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(592, 497);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(196, 27);
+            this.btnAll.TabIndex = 0;
+            this.btnAll.Text = "Show All Records";
+            this.btnAll.UseVisualStyleBackColor = true;
+            // 
+            // grdBooks
+            // 
+            this.grdBooks.AutoGenerateColumns = false;
+            this.grdBooks.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grdBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdBooks.DataSource = this.sQLBooksDBDataSetBindingSource1;
+            this.grdBooks.Location = new System.Drawing.Point(12, 34);
+            this.grdBooks.Name = "grdBooks";
+            this.grdBooks.Size = new System.Drawing.Size(757, 335);
+            this.grdBooks.TabIndex = 1;
+            // 
+            // sQLBooksDBDataSetBindingSource
+            // 
+            this.sQLBooksDBDataSetBindingSource.DataSource = this.sQLBooksDBDataSet;
+            this.sQLBooksDBDataSetBindingSource.Position = 0;
+            // 
+            // sQLBooksDBDataSet
+            // 
+            this.sQLBooksDBDataSet.DataSetName = "SQLBooksDBDataSet";
+            this.sQLBooksDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sQLBooksDBDataSetBindingSource1
+            // 
+            this.sQLBooksDBDataSetBindingSource1.DataSource = this.sQLBooksDBDataSet;
+            this.sQLBooksDBDataSetBindingSource1.Position = 0;
+            // 
+            // frmBooks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 536);
+            this.Controls.Add(this.grdBooks);
+            this.Controls.Add(this.btnAll);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmBooks";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Books Database";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmBooks_FormClosing);
+            this.Load += new System.EventHandler(this.frmBooks_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdBooks)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sQLBooksDBDataSetBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.DataGridView grdBooks;
+        private System.Windows.Forms.BindingSource sQLBooksDBDataSetBindingSource;
+        private SQLBooksDBDataSet sQLBooksDBDataSet;
+        private System.Windows.Forms.BindingSource sQLBooksDBDataSetBindingSource1;
+    }
+}
+
---
+++ searchingBooksDatabase/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace searchingBooksDatabase
+{
+    public partial class frmBooks : Form
+    {
+        public frmBooks()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection booksConnection;
+        String SQLAll;
+        Button[] btnRolodex = new Button[26];
+        private void frmBooks_Load(object sender, EventArgs e)
+        {
+            //conect to books database
+            booksConnection = new SqlConnection("Data Source=.\\SQLEXPRESS; AttachDbFilename=C:\\Users\\thawkins022713\\source\\repos\\searchingBooksDatabase\\SQLBooksDB.mdf; Integrated Security=True; Connect Timeout=30; User Instance=True");
+            booksConnection.Open();
+            // create Rolodex buttons for searching
+            int w, lStart, l, t;
+            int buttonHeight = 33; // found by trial and error
+            // search buttons
+            // determine button width - 13 on a row
+            w = Convert.ToInt32(this.ClientSize.Width / 14);
+            // center buttons on form
+            lStart = Convert.ToInt32(0.5 * (this.ClientSize.Width - 13 * w));
+            l = lStart;
+            t = grdBooks.Top + grdBooks.Height + 2;
+            // create and position 26 buttons
+            for (int i = 0; i < 26; i++)
+            {
+                // create new pushbutton
+                btnRolodex[i] = new Button();
+                btnRolodex[i].TabStop = false;
+                // set text property
+                btnRolodex[i].Text = ((char) (65 + i)).ToString();
+                // position
+                btnRolodex[i].Width = w;
+                btnRolodex[i].Height = buttonHeight;
+                btnRolodex[i].Left = 1;
+                btnRolodex[i].Top = t;
+                // give cool colors
+                btnRolodex[i].BackColor = Color.Blue;
+                btnRolodex[i].ForeColor = Color.White;
+                // add button to form
+                this.Controls.Add(btnRolodex[i]);
+                // add event handler
+                btnRolodex[i].Click += new
+            System.EventHandler(this.btnSQL_Click);
+                // next left
+                l += w;
+                if (i == 12)
+                {
+                    // move to next row
+                    l = lStart;
+                    t += buttonHeight;
+                }
+            }
+            // Build basic SQL statement
+            SQLAll = "SELECT Authors.Author,Titles.Title,Publishers.Company_Name ";
+            SQLAll += "FROM Authors, Titles, Publishers, Title_Author ";
+            SQLAll += "WHERE Titles.ISBN = Title_Author.ISBN ";
+            SQLAll += "AND Authors.Au_ID = Title_Authors.Au_ID ";
+            SQLAll += "AND Titles.PubID = Publishers.PubID ";
+            // show from and click on all records initially
+            this.Show();
+            btnAll.PerformClick();
+        }
+
+        private void frmBooks_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            booksConnection.Close();
+            booksConnection.Dispose();
+        }
+
+        private void btnSQL_Click(object sender, EventArgs e)
+        {
+            SqlCommand resultsCommand = null;
+            SqlDataAdapter resultsAdapter = new SqlDataAdapter();
+            DataTable resultsTable = new DataTable();
+            String SQLStatement;
+            // determine which button was clicked and form SQL statement
+            Button buttonClicked = (Button) sender;
+            switch (buttonClicked.Text)
+            {
+                case "Show All Records":
+                    SQLStatement = SQLAll;
+                    break;
+                case "Z":
+                    // Z clicked
+                    // Append to SQLAll to limit records to letter clicked
+                    SQLStatement = SQLAll + "AND Authors.Author > 'Z' ";
+                    break;
+                default:
+                    // Letter key other than Z clicked
+                    // Append to SQLAll to limit records to letter clicked
+                    int index = (int)
+                (Convert.ToChar(buttonClicked.Text)) - 65;
+                    SQLStatement = SQLAll + "AND Authors.Author > '" +
+                        btnRolodex[index].Text + " ' ";
+                    SQLStatement += "AND Authors.Author < '" +
+                        btnRolodex[index + 1].Text + " ' ";
+                    break;
+            }
+            SQLStatement += "ORDER BY Authors.Author";
+            // apply SQL statement
+            try
+            {
+                // establish command object and data adapter
+                resultsCommand = new SqlCommand(SQLStatement, booksConnection);
+                resultsAdapter.SelectCommand = resultsCommand;
+                resultsAdapter.Fill(resultsTable);
+                // bind grid view to data table
+                grdBooks.DataSource = resultsTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error in Processing SQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            resultsCommand.Dispose();
+            resultsAdapter.Dispose();
+            resultsTable.Dispose();
+        }
+    }
+}
